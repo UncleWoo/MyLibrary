14b723cd346d43647150a810e020420b2a973bdc
UncleWoo <mateuszkocur93@outlook.com>
2018-05-28 23:57:43 +0200

    Add data annotations

--- MyLibrary/Models/Book.cs
+++ MyLibrary/Models/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,14 +11,19 @@ namespace MyLibrary.Models
     {
         public int BookID { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
 
         public int Pages { get; set; }
 
         public double Price { get; set; }
 
+        [DisplayName("Publication Date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}", ApplyFormatInEditMode = true)]
         public DateTime PublicationDate { get; set; }
 
         public virtual Author Author { get; set; }
--- MyLibrary/Models/Category.cs
+++ MyLibrary/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace MyLibrary.Models
     {
         public int CategoryID { get; set; }
 
+        [Required]
         public string CategoryName { get; set; }
 
         public virtual ICollection<Book> Book { get; set; }
